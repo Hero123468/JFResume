28a013fc478ae0c41958ac3bb556251a9f680ed4
Hero123468 <jessefalleur@gmail.com>
2025-06-02 17:40:41 -0500

    updated jason and vite.config.ts 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 👈 Replace with your repo name if it's different
+  // Replace with your repo name if it's different
+  base: '/jfresume/', 
   plugins: [react()],
-+  base: '/vite-deploy-demo/'
 });
