5eed7ca9255edd7600a56e2c493f2d3d7da01791
Hero123468 <jessefalleur@gmail.com>
2025-06-18 17:51:17 -0500

    got code to work again

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Routes, Route, Link, Navigate, useLocation } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home/Home'
 import Ecom from './pages/Ecom/Ecom'
 import Feature from './pages/Feature/Feature'
 import Resume from './pages/Resume/Resume'
 import { useEffect, useState } from 'react';
-import Footer from './Footer/footer'
+//import Footer from './Footer/footer'
 import './App.css'; // Import CSS file for app styling
 
 export default function App() {
-  const location = useLocation(); 
+  //const location = useLocation(); 
   //console.log("reactlocationis ",location)
   //console.log("windowlocationis ",window)
   
@@ -19,14 +19,8 @@ export default function App() {
         const handlePopState = () => {
             thingthatchangesurl(window.location.pathname);
         };
+        console.log("url is",url) 
         window.addEventListener('popstate', handlePopState);
-  function App() {
-       /*return (
-         <div className="app-container">
-           {/* Your other components/content here }
-           <Footer />
-         </div>
-       );*/
 
         return () => {
             window.removeEventListener('popstate', handlePopState);
