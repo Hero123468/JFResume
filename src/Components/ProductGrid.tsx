bc7a70996a42549bebe34af1376002bb25a33c93
Hero123468 <jessefalleur@gmail.com>
2025-07-02 16:48:57 -0500

    ProductGrid.tsx update

--- src/Components/ProductGrid.tsx
+++ src/Components/ProductGrid.tsx
@@ -1,15 +1,21 @@
-import type React from "react";
+import React from "react";
+import { products } from "../Data/products";
+import { ProductCard } from "../Components/ProductCard";
 
 export const ProductGrid: React.FC = () => {
-    return (
-        <div>
-        style={{
-            fvtdisplay: "flex",
-            flexWrap: "wrap",
-            gap: "24px",
-            justifyContent: "center",
-            padding: "24px"          
-        }}
-        </div> 
-    )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "24px",
+        justifyContent: "center",
+        padding: "24px",
+      }}
+    >
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
