5376074e0cdcb0ac6ee76ad5ded6b92f2b2e69e5
Hero123468 <jessefalleur@gmail.com>
2025-07-28 13:55:31 -0500

    assive deletion of js

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import './index.css';
-import App from './App.tsx';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(BrowserRouter, { basename: '/JFResume/', children: _jsx(App, {}) }) }));
