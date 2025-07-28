bc19026470b8dee0f4d1576c62d1659b30be3a7e
Hero123468 <jessefalleur@gmail.com>
2025-07-28 13:53:59 -0500

    update app.json

---
+++ src/Data/products.js
@@ -0,0 +1,16 @@
+export const products = [
+    {
+        id: "1",
+        title: "Soy Protein Shake – Vanilla",
+        image: "https://via.placeholder.com/260x200.png?text=Soylent+Vanilla",
+        price: 34.99,
+        link: "https://www.amazon.com/",
+    },
+    {
+        id: "2",
+        title: "Soy Protein Shake – Chocolate",
+        image: "https://via.placeholder.com/260x200.png?text=Soylent+Chocolate",
+        price: 34.99,
+        link: "https://www.amazon.com/",
+    },
+];
