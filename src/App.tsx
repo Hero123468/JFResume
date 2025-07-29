bfbe0be0366b3d7a3b1c3f67604c71f1480ee373
Hero123468 <jessefalleur@gmail.com>
2025-07-29 12:39:52 -0500

    update all

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import Home from './pages/Home/Home'
-import Ecom from './pages/Ecom/Ecom'
-import Feature from './pages/Feature/Feature'
-import Resume from './pages/Resume/Resume'
-import { useEffect, useState } from 'react';
-import Footer from './Footer/footer'
-import Header from './Header/header'
-import './App.css'; // Import CSS file for app styling
+import Ecom from './pages/Ecom/Ecom';
+import Feature from './pages/Feature/Feature';
+import Resume from './pages/Resume/Resume';
+import Login from './pages/Login/Login';
+import Navbar from './Components/src/components/Navbar';
+import Footer from './Footer/footer';
 
 export default function App() {
-  //const location = useLocation(); 
-  //console.log("reactlocationis ",location)
-  //console.log("windowlocationis ",window)
-  
-
-  const [url,thingthatchangesurl] = useState("");
- useEffect(() => {
-        const handlePopState = () => {
-            thingthatchangesurl(window.location.pathname);
-        };
-        console.log("url is",url) 
-        window.addEventListener('popstate', handlePopState);
-
-        return () => {
-            window.removeEventListener('popstate', handlePopState);
-        };
-    }, []) ;
- 
-  // useEffect(
-  //   ()=>{
-  //     thingthatchangesurl(location.pathname)
-  //     console.log(url)
-  //   },
-  //   [location]
-  // )
-
   return (
-    <div>
-      <Header/>
-      <Routes>
-        {/* Default page */}
-        <Route path="/" element={<Home />} /> 
-        <Route path="/Ecom" element={< Ecom />} />
-        <Route path="/feature" element={< Feature />} />
-        <Route path="/Resume" element={< Resume/>} />
-        
-        {/* Redirect unknown paths */}
-      {/* <Route path="*" element={<Navigate to="/" />} />  */}
-    
-      </Routes>
-      <Footer/>
-    </div>
+    <div className="app-container">
+      <Navbar />
+      
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Ecom" element={<Ecom />} />
+          <Route path="/Feature" element={<Feature />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </div>
 
-  )
+      <Footer />
+    </div>
+  );
 }
